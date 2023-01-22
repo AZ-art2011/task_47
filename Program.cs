568a1bce9218113f3ecb4a01d1afc3a28a37d58c
Alexander <mypost@example.com>
2023-01-22 21:12:29 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Clear();
+
+Console.WriteLine("Укажите число строк в массиве");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите количество столбцов в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+
+float[,] matrix = new float[m, n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        float rand = new Random().Next(-100, 100);
+        float rnd = rand / 10;
+        matrix[i, j] = rnd;
+    }
+    
+    // Console.WriteLine(rnd);
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    
+    Console.WriteLine(" ");
+}
